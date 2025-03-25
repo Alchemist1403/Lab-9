fb9d7daf9cb8ef7ac95053a99a2dfefbf04ed512
Alchemist <Alchemist1403@yandex.ru>
2025-03-26 01:30:38 +0300

    New files

--- main.py
+++ main.py
@@ -1,75 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
-app = Flask('Furniture store')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///portfolio.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 
-# products = [
-#     {'prod_name': 'sofa',
-#      'price': 12000,
-#      'in_stock': False,
-#      'id': 0},
-#     {'prod_name': 'table',
-#      'price': 6000,
-#      'in_stock': True,
-#      'id': 1},
-#     {'prod_name': 'chair',
-#      'price': 8000,
-#      'in_stock': False,
-#      'id': 2},
-# ]
-
-
-class Product(db.Model):
+class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    prod_name = db.Column(db.String(300))
-    price = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
+    title = db.Column(db.String(400), nullable = False)
+    link = db.Column(db.String(400), unique = True, nullable = False)
 
-    def __repr__(self):
-        return f'Product{self.id}. {self.prod_name} - {self.price} rub.'
+    def to_dict(self):
+        return {"id":self.id, "title":self.title, "link": self.link,}
 
 
+# Создание базы данных
+with app.app_context():
+    db.create_all()
+
+
+# Фронтенд
 @app.route('/')
-def main():
-    products = Product.query.all()
-    return render_template('index.html', products_list=products)
+def index():
+    return render_template('index.html')
 
 
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    product = Product.query.get(product_id)
-    product.in_stock = request.json['in_stock']
+# Список всех проектов
+@app.route("/api/portfolio", methods=["GET"])
+def get_projects():
+    projects = Portfolio.query.all()
+    return jsonify([project.to_dict() for project in projects])
+
+
+# Добавление проекта
+@app.route("/api/portfolio", methods=["POST"])
+def add_project():
+    data = request.get_json()
+    new_project = Portfolio(title=data["title"], link=data["link"])
+    db.session.add(new_project)
     db.session.commit()
-    # global products
-    # in_stock = request.json['in_stock']
-    # for product in products:
-    #     if product['id'] == product_id:
-    #         product.update({'in_stock': in_stock})
-    # return 'OK'
+    return jsonify(new_project.to_dict()), 201
 
 
-@app.route('/add', methods=['POST'])
-def add_product():
-    data = request.json
-    product = Product(**data)
-    db.session.add(product)
+# Удаление проекта
+@app.route("/api/portfolio/<int:project_id>", methods=["DELETE"])
+def delete_project(project_id):
+    project = Portfolio.query.get(project_id)
+    db.session.delete(project)
     db.session.commit()
-
-    # id_last = products[-1]['id']
-    # id_new = id_last + 1
-    # data['id'] = id_new
-    # products.append(data)
-    return 'OK'
-
+    return  jsonify({"message": "Запись о проекте удалена"})
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
--- static/update.js
+++ static/update.js
@@ -1,22 +1,49 @@
-function updateProd(el) {
-    product_id = el.value
-    fetch('/in_stock/' + product_id, {
-        method: 'patch',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'in_stock': el.checked})
-    })
-    console.log(product_id)
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const projectList = document.getElementById("projectList");
+    const projectForm = document.getElementById("projectForm");
 
-function addProduct() {
-    let prodName = document.getElementById('prod_name').value
-    let price = document.getElementById('price').value
-    fetch('/add', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'prod_name': prodName,
-                             'price': price,
-                             'in_stock': true})
-    })
-//    console.log("Add")
-}
+    
+    // Функция загрузки списка добавляемых проектов
+    function loadProjects() {
+        fetch("/api/portfolio")
+            .then(response => response.json())
+            .then(portfolio => {
+                projectList.innerHTML = "";
+                portfolio.forEach(project => {
+                    const li = document.createElement("li");
+                    li.innerHTML = `${project.title}. \nСсылка: ${project.link}
+                        <button onclick="deleteProject(${project.id})">Удалить</button>`;
+                    
+                    projectList.appendChild(li);
+                });
+            });
+    }
+
+
+    // Добавление записи о новом проекте
+    projectForm.addEventListener("submit", event => {
+        event.preventDefault();
+        const title = document.getElementById("title").value;
+        const link = document.getElementById("link").value;
+
+        fetch("/api/portfolio", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ title, link })
+        }).then(response => response.json())
+          .then(() => {
+              projectForm.reset();
+              loadProjects();
+          });
+    });
+
+    // Удаление записи о проекте
+    window.deleteProject = (id) => {
+        fetch(`/api/portfolio/${id}`, { method: "DELETE" }).then(() => loadProjects());
+    };
+
+
+    // Загрузка проектов при загрузке страницы
+    loadProjects();
+
+});
